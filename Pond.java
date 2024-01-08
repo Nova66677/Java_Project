3e6d94ad2f8973fb67693b6346af9594033c9e99
Nova66677 <133113356+Nova66677@users.noreply.github.com>
2024-01-08 13:28:05 +0100

    Implantation main menu

--- Pond.java
+++ Pond.java
@@ -1,5 +1,6 @@
 package MyPackage;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 
@@ -25,6 +26,16 @@ public class Pond {
 	 * 
 	 */
 	
+	
+	
+    /**
+     * Crée une grenouille avec des caractéristiques aléatoires.
+     * 
+     * @param nbFrog    Le numéro de la grenouille (int).
+     * @param limite_x  La limite en x pour la position aléatoire (int).
+     * @param limite_y  La limite en y pour la position aléatoire (int).
+     * @return La grenouille créée avec des caractéristiques aléatoires (Frog).
+     */
 	public static Frog createFrog(int nbFrog, int limite_x, int limite_y) {
 		int speed = 3;
     	int limInfPortee = 1;
@@ -44,6 +55,15 @@ public class Pond {
 		return frog;
 	}
 	
+	
+    /**
+     * Génère une liste de grenouilles avec des caractéristiques aléatoires.
+     * 
+     * @param nbFrog    Le nombre de grenouilles à générer (int).
+     * @param limite_x  La limite en x pour la position aléatoire (int).
+     * @param limite_y  La limite en y pour la position aléatoire (int).
+     * @return La liste des grenouilles générées (ArrayList<Frog>).
+     */
 	public static ArrayList<Frog> generateFrog(int nbFrog, int limite_x, int limite_y) {
 		ArrayList<Frog> array_frog = new ArrayList<>();
 		for (int i=0; i<nbFrog; i++) {
@@ -53,6 +73,14 @@ public class Pond {
 	}
 	
 	
+    /**
+     * Crée une mouche avec des caractéristiques aléatoires.
+     * 
+     * @param nbFly     Le numéro de la mouche (int).
+     * @param limite_x  La limite en x pour la position aléatoire (int).
+     * @param limite_y  La limite en y pour la position aléatoire (int).
+     * @return La mouche créée avec des caractéristiques aléatoires (Fly).
+     */
 	public static Fly createFly(int nbFly, int limite_x, int limite_y) {
 		
 		int speed = 2; // Une mouche est moins rapide qu'une grenouille 
@@ -69,6 +97,14 @@ public class Pond {
 	}
 	
 	
+    /**
+     * Génère une liste de mouches avec des caractéristiques aléatoires.
+     * 
+     * @param nbFly     Le nombre de mouches à générer (int).
+     * @param limite_x  La limite en x pour la position aléatoire (int).
+     * @param limite_y  La limite en y pour la position aléatoire (int).
+     * @return La liste des mouches générées (ArrayList<Fly>).
+     */
 	public static ArrayList<Fly> generateFly(int nbFly, int limite_x, int limite_y) {
 		ArrayList<Fly> array_fly = new ArrayList<>();
 		for (int i=0; i<nbFly; i++) {
@@ -78,6 +114,13 @@ public class Pond {
 	}
 	
 	
+    /**
+     * Calcule la distance entre une grenouille et une mouche.
+     * 
+     * @param frog La grenouille (Frog).
+     * @param fly  La mouche (Fly).
+     * @return La distance entre la grenouille et la mouche (double).
+     */
 	public static double distance(Frog frog, Fly fly) {
 		int xFly = fly.getX();
 		int yFly = fly.getY();
@@ -88,6 +131,14 @@ public class Pond {
 		return d;
 	}
 	
+	
+    /**
+     * Déplace toutes les grenouilles et toutes les mouches pour un certain nombre de tours.
+     * 
+     * @param nbTour      Le nombre de tours de déplacement (int).
+     * @param Frog_array  La liste des grenouilles (ArrayList<Frog>).
+     * @param Fly_array   La liste des mouches (ArrayList<Fly>).
+     */
 	public static void move_all(int nbTour, ArrayList<Frog> Frog_array, ArrayList<Fly> Fly_array) {
 		int dx, dy;
 		int min = -1;
@@ -127,14 +178,21 @@ public class Pond {
         ArrayList<Frog> Frog_array = new ArrayList<>();
         ArrayList<Fly> Fly_array = new ArrayList<>();
         
-        Fly_array = Pond.generateFly(10, taille_y, taille_x);
-        Frog_array = Pond.generateFrog(10, taille_y, taille_x);
+        // Initialisation 
+        
+        System.out.println("<---- Bienvenue dans l'étang !---->");
+        System.out.println("Please enter the desired number of frogs : ");
+        Scanner myInput = new Scanner(System.in);  // On créé notre input
+        int nb_frog = Integer.valueOf(myInput.nextLine());
+        System.out.println("Please enter the desired number of fly : ");
+        int nb_fly = Integer.valueOf(myInput.nextLine());
+        
+        Fly_array = Pond.generateFly(nb_fly, taille_y, taille_x);
+        Frog_array = Pond.generateFrog(nb_frog, taille_y, taille_x);
         
         
         Frog.species = "1331 Frogs";
         
-        // Initialisation 
-        System.out.println("<---- Bienvenue dans l'étang !---->");
         int tour = 0;
         boolean res = false; // On init à une valeure quelconque
         double d;
@@ -143,8 +201,17 @@ public class Pond {
         
         Pond.move_all(10000, Frog_array, Fly_array);
         
+        System.out.println("Enter 'start' to start the simulation");
+        String test = myInput.nextLine();
+        while (test != "start") {
+        	System.out.println("Enter 'start' to start the simulation");
+            test = myInput.nextLine();
+            System.out.println(test);
+        }
+        System.out.println("<--- Starting simulation--->");
+        
         while (tour < 30) {
-        	System.out.println("iter "+tour);
+        	System.out.println("iter " + tour);
         	// On déplace les grenouilles
         	Pond.move_all(1, Frog_array, Fly_array);
         	
