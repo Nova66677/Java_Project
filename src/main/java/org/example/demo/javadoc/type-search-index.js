dc875c77ad15d8fd9c1b87ab69f4a1d10ec2e383
Nova66677 <133113356+Nova66677@users.noreply.github.com>
2024-01-09 21:27:48 +0100

    add javadoc

---
+++ src/main/java/org/example/demo/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.demo","l":"Animal"},{"p":"org.example.demo","l":"Fly"},{"p":"org.example.demo","l":"Frog"},{"p":"org.example.demo","l":"graphics"},{"p":"org.example.demo","l":"Pond"}];updateSearchResults();
